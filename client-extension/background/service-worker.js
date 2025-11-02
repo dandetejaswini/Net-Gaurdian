fef16331558d060142a832d982b4170b5a1869c4
Dande Tejaswini <dandetejaswini1329@gmail.com>
2025-11-02 11:11:25 +0530

    Delete client-extension directory

--- client-extension/background/service-worker.js
+++
@@ -1,9 +0,0 @@
-// background/service-worker.js
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('NETGUARDIAN installed');
-});
-
-// Simple message handler
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === 'ping') sendResponse({ pong: true });
-});
